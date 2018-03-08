327ef98e0338f8aaa63b0a61837eaf775c671327
jgrant95 <jongrant95@gmail.com>
2018-03-08 21:54:17 +0000

    More expreimenting with the home content

--- hello-mac/src/components/HomeContent.vue
+++ hello-mac/src/components/HomeContent.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="content-wrapper">
-      <div class="welcome">WELCOME</div>
-      <div class="top-three-locations">TOP THREE LOCS</div>
+      <div class="subtitle">toilets say a lot about a place.</div>
+      <div class="welcome" style="display: flex; flex-direction: column;">
+            <div class="text">
+                Hello and welcome to the most amazing toilet reviewing website in the world. Everything is here and readily available for you. So what are you waiting for, go ahead; review and rate that toilet!
+            </div>
+        </div>
+      <div class="top-three-locations">
+          <div style="margin: auto;">TOP THREE PLACES</div>
+          <TopThreeItems/>
+        </div>
+        <div class="top-toilet">
+          <div style="margin: auto;">TOP TOILET</div>
+          <TopThreeItems/>
+        </div>
       <div class="categories">CATEGORIES</div>
       <div class="top-two-toilets">TOP TOILETS</div>
       <div class="more-info">MORE INFO</div>
@@ -17,11 +29,13 @@
 <script>
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
+import TopThreeItems from './TopThreeItems'
 
 export default {
   name: 'HomeContent',
   components: {
-    Icon
+    Icon,
+    TopThreeItems
   },
   data () {
     return {
@@ -32,6 +46,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$content-item-inner-padding: 10px;
+$content-grid-spacing: 1.0em;
+
 .content-wrapper {
     position: relative;
     padding: 0 24px;
@@ -39,47 +56,87 @@ export default {
     max-width: 1280px;
     margin: 0 auto;
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-auto-rows: minmax(200px, 200px);
+    grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: minmax(86px, 86px);
+    grid-gap: $content-grid-spacing;
+    margin: $content-grid-spacing auto;
+}
+
+.subtitle{
+    padding: $content-item-inner-padding;
+    grid-column-start: 1;
+    grid-column-end: 4;
+    grid-row-start: 1;
+    grid-row-end: 2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 3em;
 }
 
 .welcome {
+    padding: $content-item-inner-padding;
     grid-column-start: 1;
     grid-column-end: 2;
-    grid-row-start: 1;
-    grid-row-end: 3;
-    background-color: lightgreen;
+    grid-row-start: 2;
+    grid-row-end: 5;
+    .text{
+        justify-content: space-evenly;
+        align-items: center; 
+        font-size: 1.3em; 
+        margin: auto 0;
+    }
+}
+
+.top-toilet {
+    padding: $content-item-inner-padding;
+    grid-column-start: 3;
+    grid-column-end: 4;
+    grid-row-start: 2;
+    grid-row-end: 5;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
 }
 
 .top-three-locations {
+    padding: $content-item-inner-padding;
     grid-column-start: 2;
-    grid-column-end: 4;
-    grid-row-start: 1;
-    grid-row-end: 3;
-    background-color: lightblue;
+    grid-column-end: 3;
+    grid-row-start: 2;
+    grid-row-end: 5;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
 }
 
 .categories {
+    padding: $content-item-inner-padding;
     grid-column-start: 1;
-    grid-column-end: 4;
-    grid-row-start: 3;
-    grid-row-end: 4;
+    grid-column-end: 3;
+    grid-row-start: 5;
+    grid-row-end: 7;
     background-color: lightpink;
+    border-radius: 4px;
 }
 
 .top-two-toilets {
+    padding: $content-item-inner-padding;
     grid-column-start: 1;
     grid-column-end: 2;
-    grid-row-start: 4;
-    grid-row-end: 6;
+    grid-row-start: 7;
+    grid-row-end: 10;
     background-color: lightcyan;
+    border-radius: 4px;
 }
 
 .more-info {
+    padding: $content-item-inner-padding;
     grid-column-start: 2;
-    grid-column-end: 4;
-    grid-row-start: 4;
-    grid-row-end: 6;
+    grid-column-end: 3;
+    grid-row-start: 7;
+    grid-row-end: 10;
     background-color: lightseagreen;
+    border-radius: 4px;
 }
 </style>
---
+++ hello-mac/src/components/TopThreeItems.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="item-wrapper">
+    <div class="gradient-item" style="background: linear-gradient(90deg, #69b7eb, #b3dbd3, #f4d6db);">
+      COVENTRY
+    </div>
+    <div class="gradient-item" style="background: linear-gradient(90deg, #cfecd0, #a0cea7, #9ec0db);">
+      BIRMINGHAM
+    </div>
+    <div class="gradient-item" style="background: linear-gradient(90deg, #ee5c87, #f1a4b5, #d587b3);">
+      LINCOLN
+    </div>
+  </div>
+</template>
+
+<script>
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
+export default {
+  name: 'TopThreeItems',
+  components: {
+    Icon
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.item-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: auto 0;
+  height: 90%;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  
+  .gradient-item {
+    flex: 1;
+    width: 100%;
+    margin: 8px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;    
+    border-radius: 4px;
+    font-size: 1.5em;
+  }
+}
+</style>
